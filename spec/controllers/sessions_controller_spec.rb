c70026c001ba154d3ea2a3acb93d6adca65263a9
Kentaro Kojima <contact@toushimi.jp>
2019-05-21 22:38:43 +0900

    redirect_toに変更したテストを追加(#n_m_db_practice-15 loginに失敗したときエラーが出る)

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -17,4 +17,28 @@ RSpec.describe SessionsController, type: :controller do
 
   end
 
+  describe "POST #create" do
+    it 'redirect to login_path when can logged in' do
+      user = create(:user)
+      post :create, params: {
+          session: {
+              username: user.email,
+              password: user.password,
+          }
+      }
+      expect(response).to redirect_to mypage_path
+    end
+    it 'redirect to login_path when cannot logged in' do
+      user = create(:user)
+      post :create, params: {
+          session: {
+              username: user.email,
+              password: "abcdefg",
+          }
+      }
+      expect(response).to redirect_to login_path
+    end
+
+  end
+
 end
