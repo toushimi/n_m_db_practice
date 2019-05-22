c60513c13d14748f2882a02545d9016491064c30
Kentaro Kojima <contact@toushimi.jp>
2019-05-22 21:49:30 +0900

    Eventモデルテスト項目作成(#n_m_db_practice-20 Eventページの作成)

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe Event, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'create' do
+    before do
+      @event = build(:event)
+    end
+    it 'is valid to create event with name,place,date'
+    it 'is invalid to create event with empty name'
+    it 'is invalid to create event with empty date'
+    it 'is invalid to create event with duplicate name in same group'
+    it 'is invalid to create event with duplicate date in same group'
+  end
+
+  describe 'join_leave_unanswered' do
+    before do
+      @event = build(:event)
+    end
+    it 'is valid to user joins to answer'
+    it 'is invalid to member join answer'
+    it 'is valid to user leaves to answer'
+    it 'is invalid to leaved user leave answer'
+  end
 end
