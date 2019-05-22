20cef0ed53e591551320433671bf65973b7cc47d
Kentaro Kojima <contact@toushimi.jp>
2019-05-22 21:58:57 +0900

    EventSendMailモデルテスト項目作成(#n_m_db_practice-20 Eventページの作成)

--- spec/models/event_send_mail_spec.rb
+++ spec/models/event_send_mail_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe EventSendMail, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @event = create(:event)
+    @event_send_mail = build(:event_send_mail)
+  end
+
+  it 'is valid mail with send_to,subject,body'
+  it 'is invalid mail with empty send_to'
+  it 'is invalid mail with not exist send_to user'
+  it 'is invalid mail with empty subject'
+  it 'is invalid mail with empty body'
 end
